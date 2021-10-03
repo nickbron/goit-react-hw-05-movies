3454a3480ad0ce6b5dd554945ac0603e97568990
nickbron <77615583+nickbron@users.noreply.github.com>
2021-10-03 17:25:43 +0300

    Updates

---
+++ static/js/9.b72ad5e9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[9],{75:function(t,e,i){"use strict";i.r(e),i.d(e,"default",(function(){return c}));var n=i(1);function c(){return Object(n.jsx)("h1",{children:"404 \u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430 :("})}}}]);
+//# sourceMappingURL=9.b72ad5e9.chunk.js.map
